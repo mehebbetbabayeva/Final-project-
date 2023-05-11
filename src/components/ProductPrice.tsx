8cfc287985cb11c69d45dd8fa5921bb1dfa0c990
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-05-12 00:18:58 +0400

    add select

--- src/components/ProductPrice.tsx
+++ src/components/ProductPrice.tsx
@@ -1,6 +1,6 @@
 import {IoIosArrowUp,IoIosArrowDown} from "react-icons/io";
 import card1 from "../assets/img/cart-1.jpg.webp";
-
+import Button from "../components/Button";
  
 const ProductPrice = () => {
   return (
@@ -17,7 +17,7 @@ const ProductPrice = () => {
                      </tr>
                      </thead>
                    <tbody>
-                   <tr>
+                     <tr>
                       <td>
                         <div className="product-description">
                         <img src={card1} alt="img" />
@@ -71,18 +71,56 @@ const ProductPrice = () => {
                         
                       </td>
                      </tr>
-
                      <tr>
                       <td>
                         <button className="update-cart">UPDATE CART</button>
                       </td>
+                      <td></td>
+                      <td></td>
                       <td >
                        <div className="coupon-code">
-                       <input type="text" placeholder="Coupon Code" />
+                       <input type="text" placeholder="Coupon Code"/>
+                       <Button text="APPLY"/>
+                       <button className="close-coupon">CLOSE COUPON</button>
                        </div>
                       </td>
-                      
-                     
+                     </tr>
+                     <tr>
+                      <td></td>
+                      <td></td>
+                      <td><h5>Subtotal</h5></td>
+                      <td><h5>$210.00</h5></td>
+                     </tr>
+                     <tr>
+                      <td></td>
+                      <td></td>
+                      <td><h5>Shipping</h5></td>
+                      <td className="shopping-area">
+                          <form action="">
+                           <span>
+                            <label htmlFor="">Flat Rate: $5.00</label>
+                            <input type="radio" name="price" id="" />
+                           </span>
+                           <span>
+                            <label htmlFor="">Free Shipping</label>
+                            <input type="radio" name="price" id="" />
+                           </span>
+                           <span>
+                            <label htmlFor="">Flat Rate: $10.00</label>
+                            <input type="radio" name="price" id="" />
+                           </span>
+                           <span>
+                            <label htmlFor="">Local Delivery: $2.00</label>
+                            <input type="radio" name="price" id="" />
+                           </span>
+                           <select name="country"  >
+                           <option value="">Select a State</option> 
+                           <option value="Azerbaijan">Azerbaijan</option>
+                           <option value="Turkey">Turkey</option>
+                           <option value="Pakistan">Pakistan</option>
+                           </select>
+                          </form>
+                      </td>
                      </tr>
                    </tbody>
                 </table>
